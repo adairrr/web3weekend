71b4f3299a9de0ff131d447ebc14a938ed80583e
Juan Castellon <juan@miset.io>
2021-05-29 02:44:35 +0200

    nft first commit

--- packages/hardhat/scripts/deploy.js
+++ packages/hardhat/scripts/deploy.js
@@ -9,7 +9,8 @@ const main = async () => {
 
   console.log("\n\n 📡 Deploying...\n");
 
-  const greeter = await deploy("Greeter", ["Hello, Hardhat!"]) // <-- add in constructor args like line 19
+  //const greeter = await deploy("Greeter", ["Hello, Hardhat!"]) // <-- add in constructor args like line 19
+  const postNft = await deploy("OurNFT") // <-- add in constructor args like line 19
 
   //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
   //const secondContract = await deploy("SecondContract")
